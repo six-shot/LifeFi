e0f82386ad05cabb3ef83ae05e9a4a394cd3c909
six-shot <okhuomonajayi54@gmail.com>
2025-07-25 02:15:48 +0100

    first commit

--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,9 @@
 export const siteConfig = {
-  name: "Stockbits",
-  url: "https://stockbits.com",
-  ogImage: "https://stockbits/og.jpg",
+  name: "LifeFi",
+  url: "https://lifefi.com",
+  ogImage: "https://lifefi/og.jpg",
   description:
-    "Stockbit makes investing in tokenized stocks and real-world assets accessible, secure, and DeFi-native. Built for everyone backed by blockchain.",
+    "LifeFi makes investing in tokenized stocks and real-world assets accessible, secure, and DeFi-native. Built for everyone backed by blockchain.",
 };
 
 export type SiteConfig = typeof siteConfig;
