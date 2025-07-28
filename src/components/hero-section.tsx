c414cc1a353a6cd976dd39c15bc0116c72a9b101
six-shot <okhuomonajayi54@gmail.com>
2025-07-28 22:11:23 +0100

    hero fix

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -11,14 +11,21 @@ export default function HeroSection() {
       <section className=" relative md:h-[982px] h-screen md:px-[60px] pt-[64px] w-full flex items-center bg-[#010106] pb-[64px]">
         <video
           src="hero.mp4"
-          className="  absolute right-0 top-0 h-full w-full object-cover z-10"
+          className=" absolute right-0 top-0 h-full w-full object-cover z-10"
           autoPlay
           loop
           muted
           playsInline
           controls={false}
           disablePictureInPicture
-          preload="metadata"
+          preload="auto"
+          {...{
+            "webkit-playsinline": "true",
+            "webkit-video-playable-inline": "true",
+            "x5-playsinline": "true",
+            "x5-video-player-type": "h5",
+            "x5-video-player-fullscreen": "false",
+          }}
         />
         <div className="max-w-[1440px] mx-auto flex flex-col md:pt-0 pt-[64px] md:justify-end items-start  w-full h-full z-20 relative md:px-0 px-5">
           <div className="">
